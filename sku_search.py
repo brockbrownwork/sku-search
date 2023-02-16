223ecf3abfb1356d45e884c92b68f07e7e3ecc44
Hopper <Glenn.Hopper@signetjewelers.com>
2023-02-16 10:08:35 -0500

    The robot made this commit. Way faster, uses streaming to get the titles, then as soon as they get the titles they figure out if it's a search result or not. If it's not a search result, it pops open the page.

--- sku_search.py
+++ sku_search.py
@@ -2,13 +2,47 @@ import requests
 import webbrowser
 import threading
 from time import sleep, time
+from bs4 import BeautifulSoup
+
+def get_title(url, timeout=10):
+    response = requests.get(url, timeout=timeout)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find the title tag and wait until it's present
+    elapsed_time = 0
+    while elapsed_time < timeout:
+        title_tag = soup.find('title')
+        if title_tag:
+            title = title_tag.string
+            return title
+        else:
+            elapsed_time += 1
+            time.sleep(1)
+
+    raise TimeoutError("Timeout reached while waiting for title")
+
+def get_titles(urls, timeout=10):
+    threads = []
+    results = {}
+    for url in urls:
+        t = threading.Thread(target=lambda: results.update({url: get_title(url, timeout=timeout)}))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return results
 
 stores = ['zales', 'zalesoutlet', 'jared', 'kay', 'peoplesjewellers']
 
 def search_sku(store_name, sku):
-    r = requests.get(f"http://www.{store}.com/search?text={sku}")
-    if not "search" in r.url:
-        webbrowser.open(r.url)
+    urls = [f"http://www.{store_name}.com/search?text={sku}" for store_name in stores]
+    titles = get_titles(urls)
+    for url, title in titles.items():
+        if "Search" not in title:
+            webbrowser.open(url)
 
 while True:
     print("Type in the SKU you'd like to search for.")
@@ -17,17 +51,12 @@ while True:
     if sku == 'q':
         break
     threads = []
-    for store in stores:
-        thread = threading.Thread(target = search_sku, args = (store, sku))
-        threads.append(thread)
-        thread.start()
-    count = 0
     print("Loading...")
-    while any([i.is_alive() for i in threads]):
-        done = [not i.is_alive() for i in threads].count(True)
-        if done != count:
-            count = done
-            print("{0}/{1} sites checked...".format(done, len(stores)))
+    thread = threading.Thread(target = search_sku, args = (stores, sku))
+    threads.append(thread)
+    thread.start()
+    for thread in threads:
+        thread.join()
     print("Done!")
     end = time()
     print(f"Time taken: {end - start} seconds.")
