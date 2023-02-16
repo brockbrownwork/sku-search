b0a4973e072eca7f3b2c05396ee90bb4e704dd22
Hopper <Glenn.Hopper@signetjewelers.com>
2023-02-16 10:35:47 -0500

    This change was created by ChatGPT. I asked it to optimize the program that it already created and this is what it made.

--- sku_search.py
+++ sku_search.py
@@ -1,6 +1,6 @@
 import requests
 import webbrowser
-import threading
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from time import sleep, time
 from bs4 import BeautifulSoup
 
@@ -15,34 +15,22 @@ def get_title(url, timeout=10):
         title_tag = soup.find('title')
         if title_tag:
             title = title_tag.string
-            return title
+            return url, title
         else:
             elapsed_time += 1
             time.sleep(1)
 
     raise TimeoutError("Timeout reached while waiting for title")
 
-def get_titles(urls, timeout=10):
-    threads = []
-    results = {}
-    for url in urls:
-        t = threading.Thread(target=lambda: results.update({url: get_title(url, timeout=timeout)}))
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    return results
-
 stores = ['zales', 'zalesoutlet', 'jared', 'kay', 'peoplesjewellers']
 
-def search_sku(store_name, sku):
-    urls = [f"http://www.{store_name}.com/search?text={sku}" for store_name in stores]
-    titles = get_titles(urls)
-    for url, title in titles.items():
-        if "Search" not in title:
-            webbrowser.open(url)
+def search_sku(sku):
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_title, f"http://www.{store}.com/search?text={sku}") for store in stores]
+        for future in as_completed(futures):
+            url, title = future.result()
+            if "Search" not in title:
+                webbrowser.open(url)
 
 while True:
     print("Type in the SKU you'd like to search for.")
@@ -50,16 +38,10 @@ while True:
     start = time()
     if sku == 'q':
         break
-    threads = []
-    print("Loading...")
-    thread = threading.Thread(target = search_sku, args = (stores, sku))
-    threads.append(thread)
-    thread.start()
-    for thread in threads:
-        thread.join()
+    search_sku(sku)
     print("Done!")
     end = time()
     print(f"Time taken: {end - start} seconds.")
-    google = input("Would you like to run a google query of the SKU? (y/n) > ")
+    google = input("Would you like to run a Google query of the SKU? (y/n) > ")
     if google.lower() == "y":
         webbrowser.open(f'https://www.google.com/search?q="{sku}"')
